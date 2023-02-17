f9dd81cd6fa552d0eb937ba90b2dd4d78e6718d7
Ritchie <ritchie.kumar@wisi.tv>
2023-02-17 10:30:34 -0800

    Progress on UI/API Requests

--- src/extension.ts
+++ src/extension.ts
@@ -28,8 +28,6 @@ async function getPrograms() {
 
   const data = await response.json();
   console.log(data);
-  console.log("Above is the second data");
-
 }
 
 
@@ -37,8 +35,7 @@ async function getPrograms() {
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  //getQuotes();
-  getPrograms();
+  // getPrograms();
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
 
@@ -46,11 +43,74 @@ export function activate(context: vscode.ExtensionContext) {
     'Congratulations, your extension "wisi-video-platform-control-api" is now active!'
   );
 
+  // create a dictionary with api endpoints and their corresponding functions
+  type ApiDictionary = {
+    [key: string]: {
+      endpoint: string;
+      description: string;
+      type?: string;
+      payload?: any;
+    };
+  };
+
+  const apis: ApiDictionary = {
+    get: {
+      endpoint: "http://192.168.137.12/sys/svc/core/api/v1/ts/in/input_1/programs.json",
+      description: "Endpoint for retrieving user data. (call getPrograms())",
+      type: "GET",
+    },
+    put: {
+      endpoint: "http://192.168.137.12/sys/svc/core/api/v1/dvp/streams/ip/sources/cfmu5tj7nsis0n51siu0",
+      description: "Provides URI for updating user data.",
+      type: "PUT",
+    },
+    post: {
+      endpoint: "http://192.168.137.12/sys/svc/core/api/v1/dvp/streams/ip/sources/cfmu5tj7nsis0n51siu0",
+      description: "Provides URI for creating new configuration data within in the chassis.",
+      type: "POST",
+    },
+    delete: {
+      endpoint: "http://192.168.137.12/sys/svc/core/api/v1/dvp/streams/ip/sources/cfmu5tj7nsis0n51siu0",
+      description: "Provides URI for deleting configuration data within in the chassis",
+      type: "DELETE",
+    },
+  };
+
+  // map apis to return a label and detail property for each api
+  const apiList = Object.keys(apis).map((api) => {
+    return {
+      label: api,
+      detail: apis[api].description,
+    };
+  });
+
+
+
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
   let disposable = vscode.commands.registerCommand(
     "wisi-video-platform-control-api.testAPI",
+
+    // make an async function to show the quick pick menu and select an API from const apis
+    async () => {
+      const selection = await vscode.window.showQuickPick(apiList, {
+        matchOnDetail: true,
+      });
+
+      if (selection === null) {
+        return;
+      }
+      // if the user selects the an option with type: GET, all the getPrograms() function
+      else if (apis[selection.label].type === "GET") {
+        getPrograms();
+      }
+      else {
+        const newSelection = await vscode.window.showQuickPick(apiList, {
+          matchOnDetail: true,
+        });
+      }
+    },
     () => {
       // The code you place here will be executed every time your command is executed
       // Display a message box to the user
